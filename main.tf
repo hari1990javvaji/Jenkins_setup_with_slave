9317d4e21acb3389f46010ae6a5ebfb17fbcb213
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-27 20:19:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "null_resource" "os_update" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum install -y java-11-openjdk-devel",
+      "sudo yum install -y java-11-openjdk",
       "sudo yum -y install wget",
       "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
       "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
