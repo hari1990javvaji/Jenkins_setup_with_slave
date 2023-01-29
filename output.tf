7457b2a1a2d20cf6e903f40535263ddd0bfb934d
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-29 14:25:16 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,9 +2,9 @@ output "jenkins-public-ip" {
   value = aws_instance.jenkins.public_ip
 }
 
-output "userdata" {
+/* output "userdata" {
   value = data.template_file.userdata_jenkins_worker_linux.rendered
-}
+} */
 
 /* output "key" {
   value     = data.aws_ssm_parameter.private_key.value
