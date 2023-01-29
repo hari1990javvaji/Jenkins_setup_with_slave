c95e7b4f63f5afcc8dd3625d65197aae0fd08dcb
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-29 17:29:59 +0530

    Update client-jenkins.tf

--- client-jenkins.tf
+++ client-jenkins.tf
@@ -70,8 +70,9 @@ resource "null_resource" "install_jenkins_slave" {
       #"sudo chown -R ec2-user:ec2-user /var/lib/jenkins/",
       #"sudo chmod 0600 /var/lib/jenkins/.ssh/id*",
       "sudo adduser jenkins",
-      "mkdir -p /home/jenkins/.ssh",
-      "cat /tmp/id_rsa.pub >> /home/jenkins/.ssh/authorized_keys",
+      "sudo mkdir -p /home/jenkins/.ssh",
+      "sudo cat /tmp/id_rsa.pub >> /home/jenkins/.ssh/authorized_keys",
+      "sudo chown jenkins:jenkins -R /home/jenkins/.ssh"
     ]
   }
 }
