b976f0bf29c6c99b40ad75327daa314de81ba881
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-29 17:46:11 +0530

    Update client-jenkins.tf

--- client-jenkins.tf
+++ client-jenkins.tf
@@ -63,7 +63,7 @@ resource "null_resource" "install_jenkins_slave" {
 
   provisioner "remote-exec" {
     inline = [
-      "sleep 10",
+      /* "sleep 10",
       #"sudo mkdir -p /var/lib/jenkins",
       #"sudo chown -R ec2-user:ec2-user /var/lib/jenkins",
       #"sudo mv /tmp/.ssh /var/lib/jenkins/ &> /dev/null",
@@ -71,8 +71,8 @@ resource "null_resource" "install_jenkins_slave" {
       #"sudo chmod 0600 /var/lib/jenkins/.ssh/id*",
       "sudo adduser jenkins",
       "sudo mkdir -p /home/jenkins/.ssh",
-      "sudo cat /tmp/id_rsa.pub >> /home/jenkins/.ssh/authorized_keys",
-      "sudo chown jenkins:jenkins -R /home/jenkins/.ssh"
+      "sudo cat /tmp/.ssh/id_rsa.pub >> /home/jenkins/.ssh/authorized_keys",
+      "sudo chown jenkins:jenkins -R /home/jenkins/.ssh" */
     ]
   }
 }
